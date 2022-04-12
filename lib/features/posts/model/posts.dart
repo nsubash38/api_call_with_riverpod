0dfee3417d6972642c1b31a38f1b36dd92882fe2
nsubash38 <neupanesubash38@gmail.com>
2022-04-12 20:45:52 +0545

    added posts model class

---
+++ lib/features/posts/model/posts.dart
@@ -0,0 +1,21 @@
+class Post {
+  Post({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+  final int userId;
+  final int id;
+  final String title;
+  final String body;
+
+  factory Post.fromJson(Map<String, dynamic> json) {
+    return Post(
+      userId: json['userId'],
+      id: json['id'],
+      title: json['title'],
+      body: json['body'],
+    );
+  }
+}
